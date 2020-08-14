d01f9964cd04d0c84c4135c814c184c95665c2e5
cc <916181377@qq.com>
2020-08-14 10:33:44 +0800

    cc

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -184,7 +184,7 @@ export default {
       display table
       height 54px
       width 58px
-      padding 0px 12px
+      margin  0px 12px
       line-height 14px
       &.current
         position relative
